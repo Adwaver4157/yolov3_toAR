e883c11bc9ad9d69480e953ce7b5ef9c46025b88
TatsukichiShibuya <tatsukichi78@gmail.com>
2020-05-21 14:49:21 +0900

    update manage_py

--- manage_data.py
+++ manage_data.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import os
 import datetime
+import random
 
 def make_image():
     class_name = input("enter the class you want to make -> ")
@@ -20,35 +21,45 @@ def make_image():
         size = len(os.listdir(class_image_path))-1
         size2 = size
 
+        _, frame = cap.read()
+        h = frame.shape[0]
+        w = frame.shape[1]
+        xmin, ymin, xmax, ymax = 0, 0, 400, 400
 
-        a, b = 25, 5 # aは描画するフレーム数、bは記録するフレーム数 (gcd(a,b)=b)
-        c = 1
+        takeSS = 10  # 10*x[ms]に一回更新
+        takeSS_flag = False
+        count = 0
+
         while True:
-            _, frame = cap.read()
-            cv2.imshow("window", frame)
 
-            if c%(a//b) == 0:
-
-                #
-                #未完成
-                #
-
-                #書き出し
-                size2 += 1
-                cv2.imwrite(class_image_path+"/img"+str(size2)+".jpg", frame)
-                with open(class_annotation_path+"/img"+str(size2)+".txt", "w") as f:
-                    f.write(str(0))
-                    f.write(",")
-                    f.write(str(0))
-                    f.write(",")
-                    f.write(str(0))
-                    f.write(",")
-                    f.write(str(0))
-                    f.write("\n")
-            c += 1
-
-            key = cv2.waitKey(1000//a)
-            if key == ord('q'):
+            # 枠の操作
+            r = random.randint(-2, 2)
+            key = cv2.waitKey(10)
+            if key == ord('a') and xmin >= 10:  # 左
+                xmin -= 8+r
+                xmax -= 8+r
+            elif key == ord('s') and ymax < h-10:  # 下
+                ymin += 8+r
+                ymax += 8+r
+            elif key == ord('d') and xmax < w-10:  # 右
+                xmin += 8+r
+                xmax += 8+r
+            elif key == ord('w') and ymin >= 10:  # 上
+                ymin -= 8+r
+                ymax -= 8+r
+            elif key == ord('n') and xmax-xmin > 10:  # 縮小
+                xmin += 3+r
+                ymin += 3+r
+                xmax -= 3+r
+                ymax -= 3+r
+            elif key == ord('m') and ymin >= 5 and xmax < w-5 and ymax < h-5 and xmin >= 5:  # 拡大
+                xmin -= 3+r
+                ymin -= 3+r
+                xmax += 3+r
+                ymax += 3+r
+            elif key == ord('k'):
+                takeSS_flag = not takeSS_flag
+            elif key == ord('q'):
                 #更新内容を追加
                 class_text.write("更新時刻:")
                 class_text.write(str(datetime.datetime.now()))
@@ -61,6 +72,33 @@ def make_image():
 
                 cv2.destroyWindow("window")
                 break
+
+            #画面の更新
+            _, frame = cap.read()
+            frame_copy = frame.copy()
+            if takeSS_flag:
+                cv2.rectangle(frame_copy, (xmin, ymin), (xmax, ymax), (0, 0, 255))
+            else:
+                cv2.rectangle(frame_copy, (xmin, ymin), (xmax, ymax), (0, 255, 0))
+            cv2.imshow("window", frame_copy)
+
+            #画面を保存
+            if count%takeSS == 0 and takeSS_flag:
+                #書き出し
+                size2 += 1
+                cv2.imwrite(class_image_path+"/img"+str(size2)+".jpg", frame)
+                with open(class_annotation_path+"/img"+str(size2)+".txt", "w") as f:
+                    f.write(str(xmin))
+                    f.write(",")
+                    f.write(str(ymin))
+                    f.write(",")
+                    f.write(str(xmax))
+                    f.write(",")
+                    f.write(str(ymax))
+                    f.write("\n")
+
+            count += 1
+
         cap.release()
     else:
         print("failed to open camera")
