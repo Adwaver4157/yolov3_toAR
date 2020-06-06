197c17ae6f7f21f78753864a5e1a22182f5ce167
TatsukichiShibuya <tatsukichi78@gmail.com>
2020-06-06 20:16:59 +0900

    modified

--- recognize_gesture.py
+++ recognize_gesture.py
@@ -6,18 +6,17 @@ from kerasyolo3.yolo import YOLO
 
 cap = cv2.VideoCapture(1)
 if cap.isOpened():
-    yolo = YOLO(model_image_size=(640, 320))
-
+    yolo = YOLO()
     while True:
         _, frame = cap.read()
-        result, label, box = yolo.detect_image(frame)
-        cv2.imshow("result", result)
-
+        #result, label, box = yolo.detect_image(frame)
+        #cv2.imshow("result", result)
+        cv2.imshow("result", frame)
         """
         label, boxを処理してジェスチャーを認識する
         """
 
-        key = cv2.waitKey(1)   
+        key = cv2.waitKey(1)
         if key == ord('q'):
             cv2.destroyAllWindows()
             cap.release()
