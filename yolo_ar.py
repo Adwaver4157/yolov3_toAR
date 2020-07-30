7175e022d7ce40e8f30d0f9a572c06db8f423ed4
TatsukichiShibuya <tatsukichi78@gmail.com>
2020-07-30 22:31:49 +0900

    modified

--- yolo_ar.py
+++ yolo_ar.py
@@ -101,7 +101,7 @@ class OpenGL():
 
         # aruco settings
         aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
-        result, mClass, mBox = self.yolo.detect_image(image)
+        result, mClass, mBox = yolo.detect_image(image)
         if mClass is not None:
             cv2.rectangle(result, (mBox[1], mBox[0]),
                           (mBox[3], mBox[2]), (0, 0, 255))
